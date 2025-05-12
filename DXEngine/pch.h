04a6f9947e5de220ec4ba67505298d05305dbbbf
Jin-SukKim <99wlstjr@gmail.com>
2025-05-13 03:35:04 +0900

    Initialize Project

---
+++ DXEngine/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef _DEBUG
+//#pragma comment(lib, "libFolder\\Debug\\libName.lib")
+#else
+//#pragma comment(lib, "libFolder\\Release\\libName.lib")
+#endif
+
+#include <Windows.h>
+#include <vector>
+#include <memory>
